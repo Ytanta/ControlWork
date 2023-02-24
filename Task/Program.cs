2e344123967be363ccb274040a28871bc1c7457f
Igor <igor>
2023-02-24 11:46:31 +0300

    Метод под созданию строчного массива

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите количество строк:");
+int Line = Convert.ToInt32(Console.ReadLine());
+string[] UserArray = CreateArraeString(Line);
+
+
+
+string[] CreateArraeString(int line)
+{
+    string[] arr = new string [line];
+    for (int i = 0; i < line; i++)
+    {
+        Console.WriteLine("Введите строку: ");
+        arr[i] = Console.ReadLine()!;
+        
+
+    }
+    return arr;
+}
+
