6d1792c571edd4601b843828d2797c3d3d72da2e
Igor <igor>
2023-02-24 11:48:59 +0300

    Метод вывода данных внесенных пользователем

--- Task/Program.cs
+++ Task/Program.cs
@@ -2,6 +2,8 @@
 Console.WriteLine("Введите количество строк:");
 int Line = Convert.ToInt32(Console.ReadLine());
 string[] UserArray = CreateArraeString(Line);
+const string quote = "\"";
+PrintArray(UserArray);
 
 
 
@@ -17,4 +19,18 @@ string[] CreateArraeString(int line)
     }
     return arr;
 }
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{quote} {arr[i]} {quote}, ");
+        else Console.Write($"{quote} {arr[i]} {quote}");
+    }
+    Console.Write("]");
+}
+
+
+
 
